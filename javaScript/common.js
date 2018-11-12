5eea415ddcda783021cad8fd92bf91f7c208ec72
totianjie <373524358@qq.com>
2018-11-12 09:47:04 +0800

    Update common.js
    添加单一事件管理观察都模式

--- javaScript/common.js
+++ javaScript/common.js
@@ -352,6 +352,61 @@ var Event = {
     data: {}
 }
 
+// 单一事件管理二  观察者模式
+function EventTarget () {
+    this.handlers = {};
+}
+
+EventTarget.prototype = {
+    constructor: EventTarget,
+    addHandler: function (type, handler) { // 添加事件  监听事件
+        if (typeof this.handlers[type] == "undefined") {
+            this.handlers[type] = [];
+        }
+        this.handlers[type].push(handler);
+    },
+    fire: function (event) { // 触发事件  发送事件
+        if (!event.target) {
+            event.target = this;
+        }
+        if (this.handlers[event.type] instanceof Array) {
+            var handlers = this.handlers[event.type];
+            for (var i = 0, len = handlers.length; i < len; i++) {
+                handlers[i](event);
+            }
+        }
+    },
+    removeHandler: function (type, handler) { // 删除事件  取消监听
+        if (this.handlers[type] instanceof Array) {
+            var handlers = this.handlers[type];
+            for (var i = 0, len = handlers.length; i < len; i++) {
+                if (handlers[i] === handler) {
+                    break;
+                }
+            }
+            handlers.splice(i, 1);
+        }
+    }
+};
+// demo
+/*
+function handleMessage (event) {
+    alert("Message received: " + event.message);
+}
+
+//创建一个新对象
+var target = new EventTarget();
+//添加一个事件处理程序
+target.addHandler("message", handleMessage);
+//触发事件
+target.fire({type: "message", message: "Hello world!"});
+//删除事件处理程序
+target.removeHandler("message", handleMessage);
+//再次，应没有处理程序
+target.fire({type: "message", message: "Hello world!"});
+* */
+
+
 /** ==================== 是否是某一个东西 ==================== */
 function type (obj) {
     var toString = Object.prototype.toString;
